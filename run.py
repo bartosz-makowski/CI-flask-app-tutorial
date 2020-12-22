11d370078025b11e8b545a2aa79c1e09df8b2fc9
Bartek_makowski <mako19895@gmail.com>
2020-12-22 00:48:15 +0000

    3rd video finished

--- run.py
+++ run.py
@@ -9,6 +9,16 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/info")
+def info():
+    return render_template("info.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
