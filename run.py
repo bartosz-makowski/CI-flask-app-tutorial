9792d17f3fd7f69988191251ba6b5266dbfe9260
Bartek_makowski <mako19895@gmail.com>
2020-12-27 22:16:05 +0000

    last video from this sub module finished

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,17 +7,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/info")
 def info():
-    return render_template("info.html")
+    return render_template("info.html", page_title="Info")
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
